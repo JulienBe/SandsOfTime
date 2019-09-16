d3a63bd351b554242bd086e41cab97fe9c5fb9b2
Bertozzi Julien <bertozzijulien@gmail.com>
2019-09-16 10:05:50 +0200

    Create GTime.kt

---
+++ core/src/main/kotlin/be/particulitis/hourglass/common/GTime.kt
@@ -0,0 +1,11 @@
+package be.particulitis.hourglass.common
+
+object GTime {
+    var playerPhase = true
+    var delta = 0f
+    var playerDelta = 0f
+    var enemyDelta = 0f
+    var time = 0f
+    var playerTime = 0f
+    var enemyTime = 0f
+}

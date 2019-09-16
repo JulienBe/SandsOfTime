2800aac2ecdf85f21565de8d01cf4623b8de4fbd
Bertozzi Julien <bertozzijulien@gmail.com>
2019-09-16 10:05:40 +0200

    Update SysTime.kt

--- core/src/main/kotlin/be/particulitis/hourglass/system/SysTime.kt
+++ core/src/main/kotlin/be/particulitis/hourglass/system/SysTime.kt
@@ -1,11 +1,33 @@
 package be.particulitis.hourglass.system
 
 import com.artemis.systems.BaseSystem
+import be.particulitis.hourglass.common.GTime
 
 class SysControl : BaseSystem() {
 
+    val phaseDuration = 3f
+    var currentPhaseTimer = 0f    
+    
     override fun processSystem() {
-
+        GTime.delta = Gdx.graphics.getDeltaTime()
+        currentPhaseTimer -= GTime.delta
+        
+        if (currentPhaseTimer <= 0f) {
+            GTime.playerPhase != GTime.playerPhase
+            currentPhaseTimer = phaseDuration
+        }
+        
+        GTime.playerDelta = computeDeltas(GTime.playerPhase)
+        GTime.enemyDelta = computeDeltas(!GTime.playerPhase)        
+        
+        GTime.playerTime += GTime.playerDelta
+        GTime.enemyTime += GTime.enemyDelta
     }
 
+    fun computeDeltas(playerPhase: Boolean): Float {
+        return if (playerPhaser)
+            GTime.delta * min(1f, currentPhaseTimer)
+        else 
+            0f
+    }
 }

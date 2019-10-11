eab90070177233e632f55d50bc85d89b60a7f985
JulienBe <bertozzijulien@gmail.com>
2019-10-11 21:50:51 +0200

    blocky draw

--- core/src/main/kotlin/be/particulitis/hourglass/common/GBatch.kt
+++ core/src/main/kotlin/be/particulitis/hourglass/common/GBatch.kt
@@ -1,23 +1,25 @@
 package be.particulitis.hourglass.common
 
 import be.particulitis.hourglass.ImgMan
+import be.particulitis.hourglass.comp.CompDraw
 import be.particulitis.hourglass.comp.CompSpace
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import kotlin.math.roundToInt
 
 class GBatch(private val img: ImgMan) : SpriteBatch(8191) {
 
-    fun draw(space: CompSpace) {
-        draw(img.square, GResolution.baseX + space.x, GResolution.baseY + space.y, space.w, space.h)
+    fun draw(space: CompSpace, draw: CompDraw) {
+        packedColor = draw.color.basicf
+        draw(GResolution.baseX + space.x, GResolution.baseY + space.y, space.w)
     }
 
-    fun draw(color: Color, x: Float, y: Float, w: Float, h: Float) {
-        packedColor = color.toFloatBits()
-        draw(img.square, GResolution.baseX + x, GResolution.baseY + y, w, h)
+    fun draw(color: GPalette, x: Float, y: Float, w: Float) {
+        packedColor = color.basicf
+        draw(GResolution.baseX + x, GResolution.baseY + y, w)
     }
 
-    fun draw(r: Float, g: Float, b: Float, x: Float, y: Float, w: Float) {
-        setColor(r, g, b, 1f)
-        draw(img.square, GResolution.baseX + x, GResolution.baseY + y, w, w)
+    fun draw(x: Float, y: Float, w: Float) {
+        draw(img.square, x.roundToInt().toFloat(), y.roundToInt().toFloat(), w.roundToInt().toFloat(), w.roundToInt().toFloat())
     }
 }

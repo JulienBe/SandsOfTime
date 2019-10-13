114b8c458aa319cce2500606bd480307dff33a45
JulienBe <bertozzijulien@gmail.com>
2019-10-13 10:30:00 +0200

    fix map and font display

--- core/src/main/kotlin/be/particulitis/hourglass/common/GBatch.kt
+++ core/src/main/kotlin/be/particulitis/hourglass/common/GBatch.kt
@@ -3,19 +3,21 @@ package be.particulitis.hourglass.common
 import be.particulitis.hourglass.ImgMan
 import be.particulitis.hourglass.comp.CompDraw
 import be.particulitis.hourglass.comp.CompSpace
-import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import kotlin.math.roundToInt
 
 class GBatch(private val img: ImgMan) : SpriteBatch(8191) {
 
     fun draw(space: CompSpace, draw: CompDraw) {
-        draw(draw.color, GResolution.baseX + space.x, GResolution.baseY + space.y, space.w)
+        draw(draw.color, space.x, space.y, space.w)
     }
 
     fun draw(color: GPalette, x: Float, y: Float, w: Float) {
         packedColor = color.scale[1]
-        draw(img.square, x.roundToInt().toFloat(), y.roundToInt().toFloat(), w.roundToInt().toFloat(), w.roundToInt().toFloat())
+        val x = (GResolution.baseX + x).roundToInt().toFloat()
+        val y = (GResolution.baseY + y).roundToInt().toFloat()
+        val w = w.roundToInt().toFloat()
+        draw(img.square, x, y, w, w)
     }
 
 }
